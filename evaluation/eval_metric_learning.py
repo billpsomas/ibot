fb3aac97d5e3da502fc428c536a56615342455a4
billpsomas <psomasbill@gmail.com>
2022-03-07 02:24:42 +0200

    update for sop

--- evaluation/eval_metric_learning.py
+++ evaluation/eval_metric_learning.py
@@ -112,7 +112,24 @@ class Cars(BaseDataset):
                 self.I += [index]
                 index += 1
 
+class SOP(BaseDataset):
+    def __init__(self, root, mode, transform = None):
+        self.root = root + '/Stanford_Online_Products'
+        self.mode = mode
+        self.transform = transform
+        if self.mode == 'train':
+            self.classes = range(0,11318)
+        elif self.mode == 'eval':
+            self.classes = range(11318,22634)
 
+        BaseDataset.__init__(self, self.root, self.mode, self.transform)
+        metadata = open(os.path.join(self.root, 'Ebay_train.txt' if self.classes == range(0, 11318) else 'Ebay_test.txt'))
+        for i, (image_id, class_id, _, path) in enumerate(map(str.split, metadata)):
+            if i > 0:
+                if int(class_id)-1 in self.classes:
+                    self.ys += [int(class_id)-1]
+                    self.I += [int(image_id)-1]
+                    self.im_paths.append(os.path.join(self.root, path))
 
 
 
@@ -153,7 +170,7 @@ if __name__ == '__main__':
         We typically set this to 1 for BEiT and 0 for models with [CLS] token (e.g., DINO).
         we set this to 2 for base-size models with [CLS] token when doing linear classification.""")
     parser.add_argument('--data_path', default='/path/to/revisited_paris_oxford/', type=str)
-    parser.add_argument('--dataset', default='roxford5k', type=str, choices=['roxford5k', 'rparis6k', 'cub', 'cars'])
+    parser.add_argument('--dataset', default='roxford5k', type=str, choices=['roxford5k', 'rparis6k', 'cub', 'cars', 'sop'])
     parser.add_argument('--multiscale', default=False, type=utils.bool_flag)
     parser.add_argument('--imsize', default=224, type=int, help='Image size')
     parser.add_argument('--pretrained_weights', default='', type=str, help="Path to pretrained weights to evaluate.")
@@ -179,6 +196,8 @@ if __name__ == '__main__':
     transform = pth_transforms.Compose([
         pth_transforms.ToTensor(),
         pth_transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        pth_transforms.Resize(256),
+        pth_transforms.CenterCrop(224),
     ])
     
     if args.dataset == 'cub':
@@ -187,6 +206,9 @@ if __name__ == '__main__':
     elif args.dataset == 'cars':
         dataset_train = Cars(args.data_path, mode="train", transform=transform)
         dataset_query = Cars(args.data_path, mode="eval", transform=transform)
+    elif args.dataset == 'sop':
+        dataset_train = SOP(args.data_path, mode="train", transform=transform)
+        dataset_query = SOP(args.data_path, mode="eval", transform=transform)
 
     #sampler = torch.utils.data.DistributedSampler(dataset_train, shuffle=False)
     '''
@@ -204,6 +226,7 @@ if __name__ == '__main__':
         batch_size=1,
         num_workers=args.num_workers,
         pin_memory=True,
+        drop_last=True,
     )
     print(f"train: {len(dataset_train)} imgs / query: {len(dataset_query)} imgs")
 
@@ -264,28 +287,51 @@ if __name__ == '__main__':
 
     if utils.get_rank() == 0:  # only rank 0 will work from now on
         # normalize features
-        query_features = nn.functional.normalize(query_features, dim=1, p=2)
-
-        K = 32
-        Y = []
+        query_features = nn.functional.normalize(query_features, dim=1, p=2)        
+        if args.dataset == 'cub' or args.dataset == 'cars':
+            K = 32
+            Y = []
+            cos_sim = torch.mm(query_features, query_features.T)
+            Y = query_labels[cos_sim.topk(1 + K)[1][:,1:]]
+            Y = Y.float().cpu()
+            query_labels = query_labels.float().cpu()
+            recall = []
+            for k in [1, 2, 4, 8, 16, 32]:
+                r_at_k = calc_recall_at_k(query_labels, Y, k)
+                recall.append(r_at_k)
+                print("R@{} : {:.3f}".format(k, 100 * r_at_k))
         
-        cos_sim = torch.mm(query_features, query_features.T)
-        
-        Y = query_labels[cos_sim.topk(1 + K)[1][:,1:]]
-        Y = Y.float().cpu()
-        query_labels = query_labels.float().cpu()
+        elif args.dataset == 'sop':
+            K = 1000
+            Y = []
+            xs = []
+            for x in query_features:
+                if len(xs)<10000:
+                    xs.append(x)
+                else:
+                    xs.append(x)
+                    xs = torch.stack(xs, dim=0)
+                    cos_sim = torch.mm(xs, query_features)
+                    y = query_labels[cos_sim.topk(1 + K)[1][:,1:]]
+                    Y.append(y.float().cpu())
+                    xs = []
+            
+            xs = torch.stack(xs, dim=1)
+            cos_sim = torch.mm(xs, query_features)
+            y = query_labels[cos_sim.topk(1 + K)[1][:,1:]]
+            Y.append(y.float().cpu())
+            Y = torch.cat(Y, dim=0)
 
-        recall = []
-        for k in [1, 2, 4, 8, 16, 32]:
-            r_at_k = calc_recall_at_k(query_labels, Y, k)
-            recall.append(r_at_k)
-            print("R@{} : {:.3f}".format(k, 100 * r_at_k))
+            recall = []
+            for k in [1, 10, 100, 1000]:
+                r_at_k = calc_recall_at_k(query_labels, Y, k)
+                recall.append(r_at_k)
+                print("R@{} : {:.3f}".format(k, 100 * r_at_k))
     dist.barrier()
 
 
 
 #AttMask CUB 
-
 '''
 R@1 : 53.022
 R@2 : 66.138
@@ -293,6 +339,16 @@ R@4 : 77.093
 R@8 : 85.685
 R@16 : 91.796
 R@32 : 95.493
+'''
+
+#AttMask CUB - with resize and randomcrop
+'''
+R@1 : 57.174
+R@2 : 69.379
+R@4 : 80.250
+R@8 : 87.779
+R@16 : 92.556
+R@32 : 96.320
 '''
 
 #iBOT CUB
@@ -305,4 +361,32 @@ R@16 : 88.403
 R@32 : 93.619
 '''
 
+#iBOT CUB - with resize and randomcrop
+'''
+R@1 : 51.384
+R@2 : 63.842
+R@4 : 74.983
+R@8 : 84.065
+R@16 : 90.766
+R@32 : 94.986
+'''
 
+#AttMask Cars 
+'''
+R@1 : 39.798
+R@2 : 50.387
+R@4 : 61.419
+R@8 : 71.504
+R@16 : 81.306
+R@32 : 89.300
+'''
+
+#iBOT Cars
+'''
+R@1 : 35.641
+R@2 : 45.960
+R@4 : 56.389
+R@8 : 67.630
+R@16 : 78.072
+R@32 : 86.767
+'''
