8d9858cafeb9bab5447afd1f43c2923f4b9270e4
billpsomas <psomasbill@gmail.com>
2022-03-07 03:26:46 +0200

    error in transpose

--- evaluation/eval_metric_learning.py
+++ evaluation/eval_metric_learning.py
@@ -21,6 +21,7 @@ import utils
 import models
 import torchvision
 import scipy
+import pandas as pd
 
 from scipy import io
 from torch import nn
@@ -131,16 +132,70 @@ class SOP(BaseDataset):
                     self.I += [int(image_id)-1]
                     self.im_paths.append(os.path.join(self.root, path))
 
+class Inshop(torch.utils.data.Dataset):
+    def __init__(self, root, mode, transform = None):
+        self.root = root + '/Inshop_Clothes'
+        self.mode = mode
+        self.transform = transform
+        self.train_ys, self.train_im_paths = [], []
+        self.query_ys, self.query_im_paths = [], []
+        self.gallery_ys, self.gallery_im_paths = [], []
+                    
+        data_info = np.array(pd.read_table(self.root +'/Eval/list_eval_partition.txt', header=1, delim_whitespace=True))[:,:]
+        #Separate into training dataset and query/gallery dataset for testing.
+        train, query, gallery = data_info[data_info[:,2]=='train'][:,:2], data_info[data_info[:,2]=='query'][:,:2], data_info[data_info[:,2]=='gallery'][:,:2]
 
+        #Generate conversions
+        lab_conv = {x:i for i,x in enumerate(np.unique(np.array([int(x.split('_')[-1]) for x in train[:,1]])))}
+        train[:,1] = np.array([lab_conv[int(x.split('_')[-1])] for x in train[:,1]])
 
+        lab_conv = {x:i for i,x in enumerate(np.unique(np.array([int(x.split('_')[-1]) for x in np.concatenate([query[:,1], gallery[:,1]])])))}
+        query[:,1]   = np.array([lab_conv[int(x.split('_')[-1])] for x in query[:,1]])
+        gallery[:,1] = np.array([lab_conv[int(x.split('_')[-1])] for x in gallery[:,1]])
 
+        #Generate Image-Dicts for training, query and gallery of shape {class_idx:[list of paths to images belong to this class] ...}
+        for img_path, key in train:
+            self.train_im_paths.append(os.path.join(self.root, 'Img', img_path))
+            self.train_ys += [int(key)]
 
+        for img_path, key in query:
+            self.query_im_paths.append(os.path.join(self.root, 'Img', img_path))
+            self.query_ys += [int(key)]
 
+        for img_path, key in gallery:
+            self.gallery_im_paths.append(os.path.join(self.root, 'Img', img_path))
+            self.gallery_ys += [int(key)]
+            
+        if self.mode == 'train':
+            self.im_paths = self.train_im_paths
+            self.ys = self.train_ys
+        elif self.mode == 'query':
+            self.im_paths = self.query_im_paths
+            self.ys = self.query_ys
+        elif self.mode == 'gallery':
+            self.im_paths = self.gallery_im_paths
+            self.ys = self.gallery_ys
 
+    def nb_classes(self):
+        return len(set(self.ys))
+            
+    def __len__(self):
+        return len(self.ys)
+            
+    def __getitem__(self, index):
+        
+        def img_load(index):
+            im = Image.open(self.im_paths[index])
+            # convert gray to rgb
+            if len(list(im.split())) == 1 : im = im.convert('RGB') 
+            if self.transform is not None:
+                im = self.transform(im)
+            return im
+        
+        im = img_load(index)
+        target = self.ys[index]
 
-
-
-
+        return im, target, index
 
 def calc_recall_at_k(T, Y, k):
     """
@@ -318,13 +373,13 @@ if __name__ == '__main__':
                 else:
                     xs.append(x)
                     xs = torch.stack(xs, dim=0)
-                    cos_sim = torch.mm(xs, query_features)
+                    cos_sim = torch.mm(xs, query_features.T)
                     y = query_labels[cos_sim.topk(1 + K)[1][:,1:]]
                     Y.append(y.float().cpu())
                     xs = []
             
             xs = torch.stack(xs, dim=1)
-            cos_sim = torch.mm(xs, query_features)
+            cos_sim = torch.mm(xs, query_features.T)
             y = query_labels[cos_sim.topk(1 + K)[1][:,1:]]
             Y.append(y.float().cpu())
             Y = torch.cat(Y, dim=0)
